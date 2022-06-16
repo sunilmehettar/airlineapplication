2ef0b540987c71857e53bfb4ba14151b85e4a7e3
Sunil Mehettar <Sunil.Mehettar@blazeclan.com>
2022-06-16 15:56:16 +0530

    initial commit with project structure

---
+++ qantas/src/main/java/com/airline/qantas/QantasApplication.java
@@ -0,0 +1,13 @@
+package com.airline.qantas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QantasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QantasApplication.class, args);
+	}
+
+}
